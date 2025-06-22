41ce3a080c334e59bf766d805e16cd4f0c7392c5
EythordoNascimento <nascimentoeythor@gmail.com>
2025-06-22 11:56:24 -0300

    enviando os primeiros exercícios

---
+++ 2_testedeprocessamento/Main.java
@@ -0,0 +1,11 @@
+ public class Main {
+    public static void main(String[] args) {
+        int x, y;
+
+       x = 5;
+       y = 2;
+
+       System.out.println(x);
+       System.out.println(y);
+    }
+ }
---
+++ areatrapezio_3/AreaTrapezio.java
@@ -0,0 +1,16 @@
+package areatrapezio_3;
+
+public class AreaTrapezio {
+
+    public static void main(String[] args) {
+        double b, B, h, area;
+    
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+    
+        area = (b + B) / 2.0 * h;
+    
+        System.out.println(area);
+    }
+}
