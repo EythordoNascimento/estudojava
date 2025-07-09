feef80088925a3ec942aa1b41dad10acc2ceafc0
EythordoNascimento <nascimentoeythor@gmail.com>
2025-07-09 08:47:38 -0300

    FOR EACH

---
+++ foreach/each.java
@@ -0,0 +1,12 @@
+package foreach;
+
+public class each {
+    public static void main(String[] args) {
+        String[] vect =  new String[] {"Maria", "Bob", "Alex"};
+
+        for (String obj : vect) {
+            System.out.println(obj);
+            
+        }
+    }
+}
